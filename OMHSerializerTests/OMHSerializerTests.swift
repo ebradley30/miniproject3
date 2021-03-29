8f70a057481c9641d8bf44a2164c354887f80a4f
ebradley30 <56734724+ebradley30@users.noreply.github.com>
2021-03-29 04:16:50 -0400

    Add files via upload

---
+++ OMHSerializerTests/OMHSerializerTests.swift
@@ -0,0 +1,34 @@
+//
+//  OMHSerializerTests.swift
+//  OMHSerializerTests
+//
+//  Created by Elaine Bradley on 3/26/21.
+//
+
+import XCTest
+@testable import OMHSerializer
+
+class OMHSerializerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        print("Put setup code here. This method is called before the invocation of each test method in the class.")
+    }
+
+    override func tearDownWithError() throws {
+        print("Put teardown code here. This method is called after the invocation of each test method in the class.")
+    }
+
+    func testExample() throws {
+        print("This is an example of a functional test case.")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(0 == 0)
+    }
+
+    func testPerformanceExample() throws {
+        print("This is an example of a performance test case.")
+        //self.measure {
+            // Put the code you want to measure the time of here.
+        //}
+    }
+
+}
